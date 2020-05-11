0c68e7f326bb1fe5336c3d65002daca4e808f25e
Snigdha <51503480+snigdha007@users.noreply.github.com>
2020-05-11 15:31:29 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+import TodoList from "./TodoList.js";
+var destination = document.querySelector("#container");
+
+ReactDom.render(
+    <div>
+        <TodoList/>
+    </div>,
+    destination
+);
